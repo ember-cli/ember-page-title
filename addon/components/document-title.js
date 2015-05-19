bdc688dc9c0dbd487a900d32138e23b25998d43c
Tim Evans <tim.c.evans@me.com>
2015-05-18 21:13:07 -0400

    use ember-wormhole to stabilize internal apis

--- addon/components/document-title.js
+++ addon/components/document-title.js
@@ -1,20 +1,25 @@
 import Ember from "ember";
+import Wormhole from "ember-wormhole/components/ember-wormhole";
 import TokenList from "../system/token-list";
 
-var tokens = new TokenList();
-var get = Ember.get;
-var set = Ember.set;
+const tokens = new TokenList();
 
-export default Ember.Component.extend({
+const get = Ember.get;
+const set = Ember.set;
+
+const computed = Ember.computed;
+
+export default Wormhole.extend({
   // Public API
   separator: null,
   title: null,
   prepend: null,
   replace: false,
-
-  isVirtual: true,
-  tagName: '',
   hidden: false,
+
+  destinationElement: computed(function () {
+    return document.getElementsByTagName('title')[0];
+  }),
 
   init: function () {
     // Clear default title
@@ -32,11 +37,19 @@ export default Ember.Component.extend({
   showSeparatorAfter: null,
   showSeparatorBefore: null,
 
+<<<<<<< HEAD
   render: function (buffer) {
     var titleTag = document.getElementsByTagName('title')[0];
     var previous = get(this, 'previous');
     var replace = get(this, 'replace');
     var dom = buffer.dom || this.renderer._dom;
+=======
+  appendToDestination() {
+    const previous = get(this, 'previous');
+    const replace = get(this, 'replace');
+    const element = get(this, 'destinationElement');
+
+>>>>>>> use ember-wormhole to stabilize internal apis
     if (previous && get(previous, 'prepend')) {
       if (get(previous, 'showSeparatorBefore')) {
         var pivot = get(previous, 'previous');
@@ -46,19 +59,30 @@ export default Ember.Component.extend({
         set(previous, 'showSeparatorBefore', false);
       }
       set(this, 'showSeparatorAfter', !replace);
+<<<<<<< HEAD
       var firstNode = previous._morph.firstNode || previous._morph.start;
       this._morph = dom.insertMorphBefore(titleTag, firstNode);
     } else {
       set(this, 'showSeparatorBefore', !replace);
       this._morph = dom.appendMorph(titleTag);
+=======
+      this.prependRange(element, this._firstNode, this._lastNode, previous._lastNode);
+    } else {
+      set(this, 'showSeparatorBefore', !replace);
+      this.appendRange(element, this._firstNode, this._lastNode);
     }
-    this._super.apply(this, arguments);
   },
 
-  willClearRender: function () {
+  prependRange(destinationElement, firstNode, lastNode, referenceElement) {
+    while (firstNode) {
+      destinationElement.insertBefore(firstNode, referenceElement);
+      firstNode = firstNode !== lastNode ? lastNode.parentNode.firstChild : null;
+>>>>>>> use ember-wormhole to stabilize internal apis
+    }
+  },
+
+  willDestroyElement() {
     tokens.remove(this);
-    var morph = this._morph;
-    Ember.run.schedule('render', morph, morph.destroy);
     this._super.apply(this, arguments);
   }
 });
