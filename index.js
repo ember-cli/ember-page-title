5f7ba38cecd25c3b8199a8e500809783e5741654
Tim Evans <tevans@movableink.com>
2016-09-15 16:04:25 -0400

    fix variable names (.____ .)

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var TranslateHelperName = require('./lib/plugins/translate-helper-name');
 module.exports = {
   name: 'ember-page-title',
 
-  setupPreprocessorRegistry: function (type, registry) {
-    app.registry.add('htmlbars-ast-plugin', {
+  setupPreprocessorRegistry: function (_, registry) {
+    registry.add('htmlbars-ast-plugin', {
       name: 'translate-title-helper-to-page-title-helper',
       plugin: TranslateHelperName,
       baseDir: function() {
