8918d709c4599b6bdc8676059f17a23a7262adb5
Tim Evans <tevans@movableink.com>
2016-09-15 16:02:31 -0400

    attempt to fix ast-plugin by putting it in the setupPreprocessorRegistry hook

--- index.js
+++ index.js
@@ -6,20 +6,15 @@ var TranslateHelperName = require('./lib/plugins/translate-helper-name');
 module.exports = {
   name: 'ember-page-title',
 
-  included: function () {
-    this._super.included.apply(this, arguments);
+  setupPreprocessorRegistry: function (type, registry) {
+    this._super.setupPreprocessorRegistry.apply(this, arguments);
 
-    // Do not add ast conversion in addons/engines
-    // This means that inside of addons/engines, you can only use {{page-title}}, not {{title}}
-    var app = this.app;
-    if (typeof app !== 'undefined' && typeof app.registry !== 'undefined') {
-      app.registry.add('htmlbars-ast-plugin', {
-        name: 'translate-title-helper-to-page-title-helper',
-        plugin: TranslateHelperName,
-        baseDir: function() {
-          return __dirname;
-        }
-      });
-    }
+    app.registry.add('htmlbars-ast-plugin', {
+      name: 'translate-title-helper-to-page-title-helper',
+      plugin: TranslateHelperName,
+      baseDir: function() {
+        return __dirname;
+      }
+    });
   }
 };
