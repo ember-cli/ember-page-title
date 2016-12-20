cedfc4af7492aafa59f882d7fe385ba4836bb95d
Tim Evans <tim_evans@condenast.com>
2016-12-20 14:12:43 -0500

    upgrade to eslint 3

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: 'module'
+  },
+  extends: 'eslint:recommended',
+  env: {
+    'browser': true
+  },
+  rules: {
+    'require-await': 1
+  }
+};
