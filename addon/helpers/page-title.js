91ef6c581b719c324505e085730d50948212b048
Nikola Begedin <begedinnikola@gmail.com>
2017-06-20 15:07:21 +0200

    Add blank object fallback for routes in integration tests

--- addon/helpers/page-title.js
+++ addon/helpers/page-title.js
@@ -33,7 +33,7 @@ export default Ember.Helper.extend({
 
     let router = getOwner(this).lookup('router:main');
     let routes = router._routerMicrolib || router.router;
-    let { activeTransition } = routes;
+    let { activeTransition } = routes || {};
     let headData = get(this, 'headData');
     if (activeTransition) {
       activeTransition.promise.finally(function () {
