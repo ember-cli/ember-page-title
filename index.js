78853db7cda98ce7b050d7dc89387a4372951c37
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-02-05 07:48:22 -0800

    fix: Allow for parallel babel builds (#159)
    Modified the pre-processor setup to use the babel parallel build syntax.

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 'use strict';
 
-var TranslateHelperName = require('./lib/plugins/translate-helper-name');
-
 module.exports = {
   name: 'ember-page-title',
 
-  setupPreprocessorRegistry: function (_, registry) {
-    registry.add('htmlbars-ast-plugin', {
+  setupPreprocessorRegistry(_, registry) {
+    const plugin = this._buildPlugin();
+
+    plugin.parallelBabel = {
+      requireFile: __filename,
+      buildUsing: '_buildPlugin',
+      params: {}
+    };
+
+    registry.add('htmlbars-ast-plugin', plugin);
+  },
+
+  _buildPlugin() {
+    return {
       name: 'translate-title-helper-to-page-title-helper',
-      plugin: TranslateHelperName,
-      baseDir: function() {
+      plugin: require('./lib/plugins/translate-helper-name'),
+      baseDir() {
         return __dirname;
       }
-    });
+    };
   }
 };
