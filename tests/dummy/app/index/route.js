d5cbf0403121e64a43bb060b1e447685013adf8e
Raido Kuli <raido357@gmail.com>
2020-10-13 09:46:27 +0300

    chore: Remove dummy app playground

--- tests/dummy/app/index/route.js
+++ tests/dummy/app/index/route.js
@@ -3,11 +3,7 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     return {
-      tokens: [{
-        value: 'ember-page-title',
-        separator: ' | '
-      }],
-      title: 'Blog',
+      title: 'Ember Page Title',
       post: {
         title: 'Ember.js'
       }
