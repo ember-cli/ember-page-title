040b3d70d6677fa88840a76bd1200683c8a5ce79
Ilya Radchenko <knownasilya@gmail.com>
2022-10-15 12:55:01 -0400

    chore: update extensions in rollup, remove ts build file

--- addon/rollup.config.js
+++ addon/rollup.config.js
@@ -15,15 +15,15 @@ export default {
     // These are the modules that users should be able to import from your
     // addon. Anything not listed here may get optimized away.
     addon.publicEntrypoints([
-      'helpers/**/*.ts',
-      'services/**/*.ts',
-      'test-support/index.ts',
+      'helpers/**/*.js',
+      'services/**/*.js',
+      'test-support/index.js',
     ]),
 
     // These are the modules that should get reexported into the traditional
     // "app" tree. Things in here should also be in publicEntrypoints above, but
     // not everything in publicEntrypoints necessarily needs to go here.
-    addon.appReexports(['helpers/**/*.ts', 'services/**/*.ts']),
+    addon.appReexports(['helpers/**/*.js', 'services/**/*.js']),
 
     // This babel config should *not* apply presets or compile away ES modules.
     // It exists only to provide development niceties for you, like automatic
