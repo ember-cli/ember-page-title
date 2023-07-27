f94e9b9f47683d77b001e812e91d679f45890711
Andy Tran <leepfrog@users.noreply.github.com>
2023-07-27 00:45:36 -0700

    docs: update tests to strictEqual

--- docs/tests/acceptance/docs-test.js
+++ docs/tests/acceptance/docs-test.js
@@ -8,12 +8,12 @@ module('Acceptance: title', function (hooks) {
 
   test('docs page renders', async function (assert) {
     await visit('/');
-    assert.equal(getPageTitle(), 'Ember Page Title');
+    assert.strictEqual(getPageTitle(), 'Ember Page Title');
   });
 
   test('user can modify title', async function (assert) {
     await visit('/');
     await fillIn('[data-test-edit-title]', 'New Title');
-    assert.equal(getPageTitle(), 'New Title');
+    assert.strictEqual(getPageTitle(), 'New Title');
   });
 });
