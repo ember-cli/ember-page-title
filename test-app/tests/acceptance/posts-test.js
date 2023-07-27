6bd2d91b58537c11dd7cb19a2f8f5e949e0bf4b5
Andy Tran <119997+leepfrog@users.noreply.github.com>
2023-07-26 22:11:27 -0700

    - test-app: re-generate from current ember-cli blueprint - test-app/tests: refactor to use strictEqual instead of equal - test-app/package.json: remove unused dependencies

--- test-app/tests/acceptance/posts-test.js
+++ test-app/tests/acceptance/posts-test.js
@@ -1,3 +1,5 @@
+/* eslint qunit/require-expect: "off" */
+
 import PageTitleService from 'ember-page-title/services/page-title';
 import { click, find, waitUntil, visit } from '@ember/test-helpers';
 import { module, test } from 'qunit';
@@ -11,81 +13,81 @@ module('Acceptance: title', function (hooks) {
     assert.expect(1);
     await visit('/posts');
 
-    assert.equal(getPageTitle(), 'Posts | My App');
+    assert.strictEqual(getPageTitle(), 'Posts | My App');
   });
 
   test('the replace attribute works', async function (assert) {
     assert.expect(1);
     await visit('/about');
 
-    assert.equal(getPageTitle(), 'About My App');
+    assert.strictEqual(getPageTitle(), 'About My App');
   });
 
   test('custom separators work', async function (assert) {
     assert.expect(1);
     await visit('/about/authors');
 
-    assert.equal(getPageTitle(), 'Authors > About My App');
+    assert.strictEqual(getPageTitle(), 'Authors > About My App');
   });
 
   test('custom separators are inherited', async function (assert) {
     assert.expect(1);
     await visit('/about/authors/profile');
 
-    assert.equal(getPageTitle(), 'Profile > Authors > About My App');
+    assert.strictEqual(getPageTitle(), 'Profile > Authors > About My App');
   });
 
   test('multiple components in a row work', async function (assert) {
     assert.expect(1);
     await visit('/posts/rails-is-omakase');
 
-    assert.equal(getPageTitle(), 'Rails is Omakase | Posts | My App');
+    assert.strictEqual(getPageTitle(), 'Rails is Omakase | Posts | My App');
   });
 
   test('the prepend=false declaration works', async function (assert) {
     assert.expect(1);
     await visit('/authors/tomster');
 
-    assert.equal(getPageTitle(), 'My App | Authors < Tomster');
+    assert.strictEqual(getPageTitle(), 'My App | Authors < Tomster');
   });
 
   test('replace nested in prepends work', async function (assert) {
     assert.expect(1);
     await visit('/hollywood');
 
-    assert.equal(getPageTitle(), 'Hollywood ★ Stars everywhere');
+    assert.strictEqual(getPageTitle(), 'Hollywood ★ Stars everywhere');
   });
 
   test('multitoken titles work', async function (assert) {
     assert.expect(1);
     await visit('/feeds/tomster');
 
-    assert.equal(getPageTitle(), 'Tomster (@tomster)');
+    assert.strictEqual(getPageTitle(), 'Tomster (@tomster)');
   });
 
   test('loading substates are not shown', async function (assert) {
     assert.expect(3);
     await visit('/feeds/tomster');
-    assert.equal(getPageTitle(), 'Tomster (@tomster)');
+    assert.strictEqual(getPageTitle(), 'Tomster (@tomster)');
 
     await click('#zoey');
     await waitUntil(() => {
       return find('div[data-test-substate-loading]') === null;
     });
-    assert.equal(getPageTitle(), 'Zoey (@zoey)');
+    assert.strictEqual(getPageTitle(), 'Zoey (@zoey)');
 
     await click('#tomster');
     await waitUntil(() => {
       return find('div[data-test-substate-loading]') === null;
     });
-    assert.equal(getPageTitle(), 'Tomster (@tomster)');
+    assert.strictEqual(getPageTitle(), 'Tomster (@tomster)');
   });
 
   test('front tokens work', async function (assert) {
     assert.expect(1);
     await visit('/reader');
 
-    assert.equal(getPageTitle(), '(10) Reader | My App');
+    assert.strictEqual(getPageTitle(), '(10) Reader | My App');
   });
 
   test('does not throw if no title element exist', async function (assert) {
@@ -93,7 +95,7 @@ module('Acceptance: title', function (hooks) {
       document.head.removeChild(titleElement);
     });
     await visit('/posts');
-    assert.equal(getPageTitle(), 'Posts | My App');
+    assert.strictEqual(getPageTitle(), 'Posts | My App');
   });
 
   test('`titleDidUpdate` hook is called with the new title', async function (assert) {
@@ -108,9 +110,9 @@ module('Acceptance: title', function (hooks) {
     this.owner.register('service:page-title', ExtendedPageTitleService);
 
     await visit('/posts');
-    assert.equal(currentTitle, 'Posts | My App');
+    assert.strictEqual(currentTitle, 'Posts | My App');
 
     await visit('/posts/rails-is-omakase');
-    assert.equal(currentTitle, 'Rails is Omakase | Posts | My App');
+    assert.strictEqual(currentTitle, 'Rails is Omakase | Posts | My App');
   });
 });
