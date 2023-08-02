7183481e2f13704b15e8126769177ed1a8fcb47b
Andy Tran <119997+leepfrog@users.noreply.github.com>
2023-08-02 16:05:16 -0700

    Add ember 3.28-lts to ember-exam

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -7,6 +7,22 @@ module.exports = async function () {
     useYarn: true,
     scenarios: [
       {
+        name: 'ember-lts-3.28',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.28.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.4',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.4.0',
+          },
+        },
+      },
+      {
         name: 'ember-lts-4.8',
         npm: {
           devDependencies: {
