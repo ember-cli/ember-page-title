6b665a64984fa611c0df745ad0b3be647c832507
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-04-11 23:31:17 -0400

    There we go. be more like the blueprint

--- addon/rollup.config.mjs
+++ addon/rollup.config.mjs
@@ -38,7 +38,10 @@ export default {
     // `dependencies` and `peerDependencies` as well as standard Ember-provided
     // package names.
     addon.dependencies(),
-    addon.declarations(),
+    addon.declarations(
+      'declarations',
+      'pnpm ember-tsc --declaration --declarationDir declarations',
+    ),
 
     // Remove leftover build artifacts when starting a new build.
     addon.clean(),
