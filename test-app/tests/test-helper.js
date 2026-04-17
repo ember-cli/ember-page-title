06eafc999d3b327f461fb27e6ef8aefb2a2be3a9
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-04-17 17:22:00 -0500

    Update test-helper(s) to use enterTestMode

--- test-app/tests/test-helper.js
+++ test-app/tests/test-helper.js
@@ -1,9 +1,10 @@
 import Application from 'test-app/app';
-import config from 'test-app/config/environment';
+import config, { enterTestMode } from 'test-app/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start as qunitStart, setupEmberOnerrorValidation } from 'ember-qunit';
 
 export function start() {
+  enterTestMode();
   setApplication(Application.create(config.APP));
 
   setupEmberOnerrorValidation();
