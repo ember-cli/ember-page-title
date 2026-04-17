75249295d05ad262460e7fc0403a8f926796e6e1
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-04-17 16:52:49 -0500

    lint:fix, remove v1 addons. lint didn't pass because our ember-cli-buildl.js files are invalid for the current lint configuration

--- docs/app/app.js
+++ docs/app/app.js
@@ -1,10 +1,10 @@
 import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
-import config from "./config/environment";
+import config from './config/environment';
 
-import setupInspector from "@embroider/legacy-inspector-support/ember-source-4.12";
-import compatModules from "@embroider/virtual/compat-modules";
+import setupInspector from '@embroider/legacy-inspector-support/ember-source-4.12';
+import compatModules from '@embroider/virtual/compat-modules';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
