785b21a0188fde1852f086744dfe730180e500fb
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-04-16 23:01:01 -0500

    Fix

--- test-app/tests/test-helper.js
+++ test-app/tests/test-helper.js
@@ -2,7 +2,9 @@ import Application from 'test-app/app';
 import config from 'test-app/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import { loadTests } from 'ember-qunit/test-loader';
 
+loadTests();
 setApplication(Application.create(config.APP));
 
 start();
