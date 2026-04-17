fd164815dea9401e0664105613bd5eef5bfaf292
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-04-17 17:32:04 -0500

    @embroider/try

---
+++ test-app/.try.mjs
@@ -0,0 +1,110 @@
+// When building your addon for older Ember versions you need to have the required files
+const compatFiles = {
+  'ember-cli-build.js': `const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const { compatBuild } = require('@embroider/compat');
+module.exports = async function (defaults) {
+  const { buildOnce } = await import('@embroider/vite');
+  let app = new EmberApp(defaults);
+  return compatBuild(app, buildOnce);
+};`,
+  'config/optional-features.json': JSON.stringify({
+    'application-template-wrapper': false,
+    'default-async-observers': true,
+    'jquery-integration': false,
+    'template-only-glimmer-components': true,
+    'no-implicit-route-model': true,
+  }),
+};
+
+  const ember4 = {
+    '@ember/test-helpers': '^3.2.1',
+    '@ember/test-waiters': '^3.0.0',
+    '@embroider/compat': '^4.0.3',
+    'ember-qunit': '^8.0.0',
+    'ember-cli': '~4.12.0',
+  };
+
+const compatDeps = {
+  '@embroider/compat': '^4.0.3',
+  'ember-cli': '^5.12.0',
+  'ember-auto-import': '^2.10.0',
+  '@ember/optional-features': '^2.2.0',
+};
+
+export default {
+  scenarios: [
+    {
+      name: 'min-supported',
+      npm: {
+        devDependencies: {
+          'ember-source': '~4.2.0',
+          ...compatDeps,
+          ...ember4,
+        },
+      },
+      env: {
+        ENABLE_COMPAT_BUILD: true,
+      },
+      files: compatFiles,
+    },
+    {
+      name: 'ember-lts-4.12',
+      npm: {
+        devDependencies: {
+          'ember-source': '~4.12.0',
+          ...compatDeps,
+          ...ember4,
+        },
+      },
+      env: {
+        ENABLE_COMPAT_BUILD: true,
+      },
+      files: compatFiles,
+    },
+    {
+      name: 'ember-lts-5.12',
+      npm: {
+        devDependencies: {
+          'ember-source': '~5.12.0',
+          ...compatDeps,
+        },
+      },
+      env: {
+        ENABLE_COMPAT_BUILD: true,
+      },
+      files: compatFiles,
+    },
+    {
+      name: `ember-lts-6.4`,
+      npm: {
+        devDependencies: {
+          'ember-source': `npm:ember-source@~6.4.0`,
+        },
+      },
+    },
+    {
+      name: `ember-latest`,
+      npm: {
+        devDependencies: {
+          'ember-source': `npm:ember-source@latest`,
+        },
+      },
+    },
+    {
+      name: `ember-beta`,
+      npm: {
+        devDependencies: {
+          'ember-source': `npm:ember-source@beta`,
+        },
+      },
+    },
+    {
+      name: `ember-alpha`,
+      npm: {
+        devDependencies: {
+          'ember-source': `npm:ember-source@alpha`,
+        },
+      },
+    },
+  ],
+};
