bf8d744672f02cd1c70c695a2088e10c42e72537
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-04-18 11:35:46 -0400

    Fix router import

--- docs/app/app.js
+++ docs/app/app.js
@@ -5,7 +5,7 @@ import setupInspector from '@embroider/legacy-inspector-support/ember-source-4.1
 export default class App extends Application {
   inspector = setupInspector(this);
   modules = {
-    './router': import.meta.glob('./router.js', { eager: true }),
+    ...import.meta.glob('./router.js', { eager: true }),
     ...import.meta.glob('./routes/**/*.js', { eager: true }),
     ...import.meta.glob('./templates/**/*.{gts,gjs}', { eager: true }),
   };
--- test-app/app/app.js
+++ test-app/app/app.js
@@ -8,7 +8,7 @@ import compatModules from "@embroider/virtual/compat-modules";
 export default class App extends Application {
   inspector = setupInspector(this);
   modules = {
-    './router': import.meta.glob('./router.js', { eager: true }),
+    ...import.meta.glob('./router.js', { eager: true }),
     ...import.meta.glob('./routes/**/*.js', { eager: true }),
     ...import.meta.glob('./templates/**/*.{gts,gjs,hbs}', { eager: true }),
   };
