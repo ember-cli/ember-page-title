b09bc033938bce4eb3a47a3b21e787b8f0142ba8
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-04-18 11:30:15 -0400

    Update testem configs to handle CI_BROWSER

--- test-app/testem.cjs
+++ test-app/testem.cjs
@@ -4,7 +4,7 @@ if (typeof module !== "undefined") {
     test_page: 'tests/index.html?hidepassed',
     cwd: 'dist',
     disable_watching: true,
-    launch_in_ci: ['Chrome'],
+    launch_in_ci: [process.env.CI_BROWSER || 'Chrome'],
     launch_in_dev: ['Chrome'],
     browser_start_timeout: 120,
     browser_args: {
