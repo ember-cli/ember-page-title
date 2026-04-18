dca249b35c86ff5e89abfbe7554f29e0f7904f1f
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-04-18 16:18:09 -0400

    Delete fastboot tests, because they are irrelevant now

--- test-app/app/app.js
+++ test-app/app/app.js
@@ -1,10 +1,7 @@
 import Application from 'ember-strict-application-resolver';
-import Resolver from 'ember-resolver';
-import config from "./config/environment";
 import { entries } from 'ember-page-title/service-registry';
 
 import setupInspector from "@embroider/legacy-inspector-support/ember-source-4.12";
-import compatModules from "@embroider/virtual/compat-modules";
 
 export default class App extends Application {
   inspector = setupInspector(this);
